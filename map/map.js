06e4f277cfa4595a3cc0c7d2782106555b4d11c9
nearxu <2448895924@qq.com>
2018-05-23 15:25:13 +0800

    Update map.js

--- map/map.js
+++ map/map.js
@@ -3,7 +3,7 @@ var item = {age:18};
 var bank = [{age:12},{age:19},{age:1}];
 
 bank = bank.map((v,i) => {
-	return v.age < 18 ? ({...v,v.active=true}):v 
+	return v.age < 18 ? ({...v,active:true}):v 
 });
 
 
